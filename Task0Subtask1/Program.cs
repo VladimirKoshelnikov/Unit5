befd598451d63fb6ccdf6c1384e9ae65c6459fc2
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-11 18:51:37 +0300

    Добавлено залание 5.0.1

--- Task0Subtask1/Program.cs
+++ Task0Subtask1/Program.cs
@@ -2,6 +2,18 @@
 
 class Programm{
     public static void Main(){
-        Console.WriteLine("Hello world");
+        (string Name, string[] Dishes) User;
+        User.Dishes = new string[5];
+
+        Console.WriteLine("Введите имя пользователя");
+        User.Name = Console.ReadLine();
+
+        for( int i = 0; i<User.Dishes.Length; i++){
+            Console.WriteLine($"Введите любимое блюдо №{i + 1} пользователя");
+            User.Dishes[i] = Console.ReadLine();   
+        }
+
+        Console.ReadKey();
+
     }
 }
