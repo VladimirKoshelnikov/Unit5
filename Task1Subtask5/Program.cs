693992dc3f8cf29ad6786955721fa4bea304ef9a
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-11 20:06:57 +0300

    Добавлены задания 5.1.5 и 5.1.6

---
+++ Task1Subtask5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class MainClass
+{
+
+    static string ShowColor(){
+        
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is default!");
+
+                    break;
+            }
+            return color;
+    }
+    public static void Main(string[] args)
+    {
+        string[] FavColor = new string[3];
+        for(int i = 0; i < FavColor.Length; i++)
+        {
+            FavColor[i] = ShowColor();
+        }
+
+        foreach (string color in FavColor)
+        {
+            Console.WriteLine($"Ваш любимый цвет {color}");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Task1Subtask6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Programm{
+
+
+    public static void Main(){
+        GetArrayFromConsole();
+    }
+
+    static int[] GetArrayFromConsole(){
+        Console.WriteLine("Введите длину массива");
+        int count = int.Parse(Console.ReadLine());
+        int[] newArray = new int[count];
+
+        Console.WriteLine("Введите массив:");
+        Console.WriteLine();
+
+        for (int i = 0; i < count; i++){
+            Console.Write("Элемент № {0}: ", i + 1);
+            newArray[i] = int.Parse(Console.ReadLine());
+        } 
+        SortArray(newArray);
+        
+        return newArray;
+    }
+  
+    static void SortArray(int[] array){
+        int temp;
+        
+        for(int i = 0; i < array.Length; i++){
+            for(int j = 0; j < array.Length; j++){
+                if (array[j] > array[i]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
